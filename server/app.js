23351d5e66e03f2e3aaf3e476e8c52d00cf41957
rishhii20 <157447444+rishhii20@users.noreply.github.com>
2024-04-23 13:56:05 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const path  = require('path')
 const server  = http.createServer(app)
 const io = new Server(server , {
     cors:{
-        origin:"http://13.201.2.106:3000"
+        origin:"http://localhost:3000"
     }
 })
 
